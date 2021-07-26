056cc559aea041553d78bb6e072044df186d61f4
Tomasz Magiera <tomasz_magiera@epam.com>
2021-07-26 14:16:35 +0200

    first commit

---
+++ src/main/java/com/test/ApiTestAutomationFramework/ApiTestAutomationFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.test.ApiTestAutomationFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestAutomationFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestAutomationFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ApiTestAutomationFramework/ApiTestAutomationFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ApiTestAutomationFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestAutomationFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
